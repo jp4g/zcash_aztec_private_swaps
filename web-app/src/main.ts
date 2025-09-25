88e7f033d850578139e6f34dc9e437e0486e7f44
Harsh Bajpai <bajpaiharsh244@gmail.com>
2025-09-25 22:43:13 +0400

    feat: working account import

--- web-app/src/main.ts
+++ web-app/src/main.ts
@@ -1,12 +1,27 @@
 import initWasm, {initThreadPool, WebWallet} from "@chainsafe/webzjs-wallet"
 
-  await initWasm();
+await initWasm();
 
 const main = async () => {
 
   await initThreadPool(4);
 
   const wallet = new WebWallet("main", "https://zcash-mainnet.chainsafe.dev", 1);
+
+  const seedPhrase = import.meta.env.VITE_SEED_PHRASE;
+  if(!seedPhrase) {
+    throw new Error("VITE_SEED_PHRASE is not set");
+  }
+
+  const birthdayHeight = import.meta.env.VITE_BIRTHDAY_HEIGHT;
+  if(!birthdayHeight) {
+    throw new Error("VITE_BIRTHDAY_HEIGHT is not set");
+  }
+
+  const accountId = await wallet.create_account("account-0", seedPhrase, 1, parseInt(birthdayHeight));
+
+
+  await wallet.sync();
 
   console.log("latest block",await wallet.get_latest_block());
 
